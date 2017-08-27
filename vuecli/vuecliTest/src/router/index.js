843140697afb085c8a75b773ae08515eb5946ebb
cyclone77 <fillchar@live.com>
2017-08-27 19:32:23 +0800

    vuerouter

--- vuecli/vuecliTest/src/router/index.js
+++ vuecli/vuecliTest/src/router/index.js
@@ -4,12 +4,13 @@ import Hello from '@/components/Hello'
 import Hi from '@/components/Hi'
 import Hi1 from '@/components/Hi1'
 import Hi2 from '@/components/Hi2'
-
+import Error from '@/components/Error'
 
 
 Vue.use(Router)
 
 export default new Router({
+    mode: "history",
     routes: [{
         path: '/',
         name: 'Hello',
@@ -20,14 +21,25 @@ export default new Router({
         component: Hi,
         children: [{
             path: '/',
-            component: Hi
+            component: Hi,
+            name: "Hello/Hi",
+            beforeEnter: (to, form, next) => {
+                console.log(to);
+                console.log(form);
+                next();
+            }
         }, {
             path: 'hi1',
-            component: Hi1
+            component: Hi1,
+            name: "Hello/Hi1"
         }, {
             path: 'hi2',
-            component: Hi2
+            component: Hi2,
+            name: "测试"
         }]
 
+    }, {
+        path: "*",
+        component: Error
     }]
 })
